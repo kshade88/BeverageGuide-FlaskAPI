ebf3fdb04ae9346ce996153d9683c1da2b08f574
kyleshade <kshade88@gmail.com>
2022-01-20 17:55:21 -0500

    editted routes and worked on readme

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-export DATABASE_URL="postgresql://localhost:5432/bg_flaskapi"
+export DATABASE_URL="postgres://pshwynegytsimb:cdf9310b780c9d402c987b838340d5e"
 
 export AUTH0_DOMAIN='beverage-guide-fsnd.us.auth0.com'
 export ALGORITHMS='RS256'
